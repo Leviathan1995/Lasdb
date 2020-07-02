47d46a8b8387ab3803f40ead866418b2c1d79115
leviathan0992@gmail.com <leviathan0992@gmail.com>
2020-07-02 14:46:07 +0800

    Update .service

--- start.sh
+++ start.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-openssl genrsa -out server.key 2048
-openssl req -new -x509 -key server.key -out server.pem -days 3650
-openssl genrsa -out client.key 2048
-openssl req -new -x509 -key client.key -out client.pem -days 3650
-openssl pkcs12 -export -clcerts -in client.pem -inkey client.key -out root.p12 -passout pass:abc
-mv server.key server.pem client.key client.pem /etc/
-
 if [ "$1" = "client" ]
 then
   sudo mkdir -p /root/proxy/
   sudo cp trident-client.service /etc/systemd/system/
-  sudo cp trident-client /root/proxy/
+  sudo cp trident-client /usr/local/bin/
   sudo cp .trident-client.json /root/proxy/
   systemctl enable trident-client
   systemctl start trident-client
   systemctl status trident-client -l
 elif [ "$1" = "server" ]
 then
-  sudo mkdir -p /root/proxy/
+  openssl genrsa -out server.key 2048
+  openssl req -new -x509 -key server.key -out server.pem -days 3650
+  openssl genrsa -out client.key 2048
+  openssl req -new -x509 -key client.key -out client.pem -days 3650
+  openssl pkcs12 -export -clcerts -in client.pem -inkey client.key -out root.p12 -passout pass:abc
+  mv server.key server.pem client.key client.pem /etc/
+
   sudo cp trident-server.service /etc/systemd/system/
-  sudo cp trident-server /root/proxy/
+  sudo cp trident-server /usr/local/bin/
   sudo cp .trident-server.json /root/proxy/
   systemctl enable trident-server
   systemctl start trident-server
